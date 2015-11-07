582f80d488919636e8d3d45cbd6ef638be503d3e
David Zheng <dzd123@mit.edu>
2015-11-07 12:52:12 -0500

    Updated landing page. Prettier :)

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// set up dummy html env to use jquery in
-// we only actually use $ to create html elements programmatically
 var $ = 0;
 (function () {
   'use strict';
@@ -22,13 +20,18 @@ var $ = 0;
 var url = require('url');
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  // host     : 'localhost', // david's ip: 172.26.10.202
-  host     : '172.26.10.202',
-  user     : 'root',
-  password : '' // TODO: make this read from config
+  host     : 'localhost', // david's ip: 172.26.10.202
+  user     : 'dzd123',
+  password : ''
 });
 
-connection.connect();
+// ***TO MAKE A QUERY***
+// connection.connect();
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
 // connection.end();
 
 
@@ -41,78 +44,21 @@ var results_bot =
 "</div>" + 
 "</section>";
 
-// lenn: number of flights scrolled into view per scroll
-var len = 3; 
-
-// caching-ish stuff
-// TODO: make it a more legit real cache
-var sql_queries = {};
-var prev_query;
-var prev_result;
-
-// assign each query an id (current cnt)
-var cnt = 0;
-
-module.exports = function(request, callback) {
+var item = "<div class='item'> ITEM</div> <div class='next'><a href='/query?status=next'>next</a></div>";
+var count = 2;
+module.exports = function(request){
   var url_parts = url.parse(request.url, true);
   var query = url_parts.query; // json file {'param':value}
-  var status = query['status'];
-  var search_string = query['search_string'];
+  var status = query['status'];  
+  
+  var search_string = query['search_string'];  
 
-  var sql_query;
-  if (status == 'next') {
-    sql_query = sql_queries[query.id];
-  } else {
-    // TODO: modify this to sort by a relevancy/preference metric
-    sql_query = "SELECT * FROM flights.data WHERE origin = '" + query.origin + "'";
+  var a = $("<div class='item'>").append($("<img src='images/nyc1\ copy\ "+count+".jpg' />"));
+  var b = $("<div class='next'>").append($("<a href='/query?status=next'>").append('next'));
+  item = a[0].outerHTML + b[0].outerHTML;
+  
+  if (status == 'next'){
+    return item;
   }
-  var start = 0;
-  if (status == 'next') {
-    start = parseInt(query.start);
-  }
-
-  renderFlights = function(rows, start, len) {
-    var idx = start;
-    // ret is a temp. container; not actually rendered in the end
-    var ret = $('<div>');
-    while (idx < start + len && idx < rows.length) {
-      var row = rows[idx];
-      var el = $("<div class='item'>");
-
-      // el holds a flight. 
-      // TODO: make it pretty
-      el.append(JSON.stringify(row));
-
-      ret.append(el);
-      ++idx;
-    }
-
-    // TODO: concurrency = lol
-    ++cnt;
-    sql_queries[''+cnt] = sql_query;
-    nextParams = 'status=next&id='+cnt+'&start='+idx;
-
-    var b = $("<div class='next'>").append($("<a href='/query?" + nextParams + "'>").append('next'));
-    ret.append(b);
-    if (start == 0) {
-      return results_top+ret.html()+results_bot;
-    } else {
-      return ret.html();
-    }
-    // TODO: if idx == rows.length, return 'no more flights' aka infinite scroll -> finite page
-  }
-
-  if (prev_query == sql_query) {
-    callback(renderFlights(prev_result, start, len));
-  } else {
-    connection.query(sql_query, function(err, rows, fields) {
-      if (err) throw err;
-
-      // TODO: concurrency = lol
-      prev_query = sql_query;
-      prev_result = rows;
-
-      callback(renderFlights(prev_result, start, len));
-    });
-  }
+  return results_top+item+results_bot;
 }

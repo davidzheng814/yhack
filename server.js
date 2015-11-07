1d1cff33c63b1c677e7a867b26abd4fc1aaf163e
David Zheng <dzd123@mit.edu>
2015-11-06 22:32:30 -0500

    Added query string parsing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-count = 0
+var url = require('url');
 
 module.exports = function(request){
-    count += 1;
-    console.log(count);
+  var url_parts = url.parse(request.url, true);
+  var query = url_parts.query; // json file {'param':value}
+  console.log(query);
 }

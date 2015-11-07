9e290eefd7c9b47cfc4c2373abc5d133265c83b9
David Zheng <dzd123@mit.edu>
2015-11-06 23:08:40 -0500

    Added database connection

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 var url = require('url');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost', // david's ip: 172.26.10.202
+  user     : 'dzd123',
+  password : ''
+});
+
+// ***TO MAKE A QUERY***
+// connection.connect();
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
+// connection.end();
 
 module.exports = function(request){
   var url_parts = url.parse(request.url, true);

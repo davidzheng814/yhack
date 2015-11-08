b3da1ffb849766ceea3323c93dbf52458bcb5448
David Zheng <dzd123@mit.edu>
2015-11-07 21:48:18 -0500

    added parsing for search-string

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,41 @@
 window.onload = function() {
   window.scrollTo(0, 0);
   disableScroll();
+  var searchStringTags = {'beach':'1-1', 'exploration':'1-2', 'family':'1-3', 'nightlife':'1-4', 'romance':'1-5', 'caribbean islands':'2-3', 'puerto rico':'2-4', 'dominican republic':'2-5', 'colombia':'2-6', 'the south':'2-8', 'mountain west':'2-9', 'desert west':'2-10', 'pacific northwest':'2-11', 'midwest':'2-15', 'gulf':'2-19', 'texas':'2-20'};
+
+  if(typeof(String.prototype.trim) === "undefined")
+  {
+      String.prototype.trim = function() 
+      {
+          return String(this).replace(/^\s+|\s+$/g, '');
+      };
+  }
 
   $('.query-form').on('submit', function () {
     text_fields = ['search-string', 'from-date', 'to-date', 'origin'];
     var query_string = "";
     for (item of text_fields) {
       var val = $('#'+item).val();
-      query_string += item + '=' + $('#'+item).val() + '&';
+      if (item == 'origin') {
+        val = val.substring(val.indexOf('(') + 1, val.indexOf(')'));
+      }else if (item == 'search-string') {
+        var tokens = val.split(',');
+        val = "";
+        for (token of tokens) {
+          token = token.trim().toLowerCase();
+          if (token == '')
+            continue;
+          if (token.indexOf('(') != -1) {
+            token = token.substring(token.indexOf('(') + 1, token.indexOf(')'));
+            val += token + ',';
+          } else {
+            if (token in searchStringTags) {
+              val += searchStringTags[token] + ',';
+            }
+          }
+        }
+      }
+      query_string += item + '=' + val + '&';
     }
 
     query_string += 'max-price='+$('#amount').html() + '&';
@@ -18,7 +46,7 @@ window.onload = function() {
     $('.loading').css({
       'display':'block'
     });
-    
+
     enableScroll();
     $('html, body').animate(
       {
